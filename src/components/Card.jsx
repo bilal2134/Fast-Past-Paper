5e66d566c8e03b18213b204fd088dad565c84386
bilal shakeel <120062035+bilal2134@users.noreply.github.com>
2024-09-21 12:06:45 +0500

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,32 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-// const Card = ({ title, index }) => {
-//   return (
+const Card = ({ title, index }) => {
+  return (
 
          
 
-//     // <div className="card">
+    // <div className="card">
       
-//     //   <h3>{title}</h3>
-//     //   <Link to={`/details/${index}`}><button>Show More</button></Link>
-//     // </div>
-//   );
-// };
-
-
-
-const Card = () => {
-  return (
- <div className="roadmap">
-      <h2>for a cleaner past paper intetface</h2>
-      <a href="https://www.wondar.site/">
-        <button className="roadmap-button">Click here</button>
-      </a>
-    </div>
-    
-    
+    //   <h3>{title}</h3>
+    //   <Link to={`/details/${index}`}><button>Show More</button></Link>
+    // </div>
   );
 };
+
+
+
+// const Card = () => {
+//   return (
+//  <div className="roadmap">
+//       <h2>for a cleaner past paper intetface</h2>
+//       <a href="https://www.wondar.site/">
+//         <button className="roadmap-button">Click here</button>
+//       </a>
+//     </div>
+    
+    
+//   );
+// };
 
 export default Card;
